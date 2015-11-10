c071c29d92c565a04b18b9154c44d80d0a4b141d
ovnisoftware <info@ovnisoftware.com>
2015-11-10 18:00:08 -0500

    Initial Upload

---
+++ RealTimeChat/ChatHub.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace RealTimeChat
+{
+    public class ChatHub : Hub
+    {
+        private static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        private static List<MessageDetail> CurrentMessage = new List<MessageDetail> ();
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
+            {
+                ConnectedUsers.Add(new UserDetail { ConnectionId = id, UserName = userName });
+                
+                // send to caller
+                Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
+                
+                // send to all except caller client
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
+        }
+
+        public void Send(string name, string message)
+        {
+            // Store last 100 messages in cache
+            AddMessageinCache(name, message);
+
+            //Call the broadcastMessage method to update clients
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        private void AddMessageinCache(string userName, string message)
+        {
+            CurrentMessage.Add(new MessageDetail { UserName = userName, Message = message });
+
+            if (CurrentMessage.Count > 100)
+                CurrentMessage.RemoveAt(0);
+        }
+
+        public override Task OnConnected()
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                ConnectedUsers.Remove(item);
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.UserName);
+            }
+            var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ RealTimeChat/Scripts/ChatApp.js
@@ -0,0 +1,156 @@
+﻿$(function () {
+    // Declare a proxy to reference the hub.
+    var chat = $.connection.chatHub;
+
+    registerClientMethods(chat);
+
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        registerEvents(chat)
+    });
+});
+
+function registerClientMethods(chat) {
+    //Counts the number of users connected
+    chat.client.online = function (count) {
+        $("#onlineUsers").html(count);
+    };
+
+    $("#btnStartChat").click(function () {
+        var name = $("#txtNickName").val();
+        if (name.length > 0) {
+            chat.server.connect(name);
+            $('.chat_box').show('fast');
+            $('.msg_box').show('fast');
+            $('.login').dialog('close');
+        }
+        else {
+            alert("Please enter name");
+        }
+    });
+
+    // Calls when user successfully logged in
+    chat.client.onConnected = function (id, userName, allUsers, messages) {
+        $('#userId').val(id);
+        $('#userName').val(userName);
+
+        //Add username to msg_head
+        $('.msg_head').prepend($('#userName').val());
+
+        // Add All Users
+        for (i = 0; i < allUsers.length; i++) {
+            AddUser(chat, allUsers[i].ConnectionId, allUsers[i].UserName);
+        }
+
+        // Add Existing Messages
+        for (i = 0; i < messages.length; i++) {
+            AddMessage(messages[i].UserName, messages[i].Message);
+        }
+
+        // Set initial focus to message input box.
+        $('.msg_input').focus();
+    }
+
+    // On User Disconnected
+    chat.client.onUserDisconnected = function (id, userName) {
+
+        $('#' + id).remove();
+
+        var ctrId = 'private_' + id;
+        $('#' + ctrId).remove();
+
+        var disc = $('<div class="disconnect">"' + userName + '" logged off.</div>');
+
+        $(disc).hide();
+        $('.chat_body').prepend(disc);
+        $(disc).fadeIn(200).delay(2000).fadeOut(200);
+    }
+
+    // On New User Connected
+    chat.client.onNewUserConnected = function (id, name) {
+        AddUser(chat, id, name);
+    }
+
+    function AddMessage(userName, message) {
+        $('.msg_body').append('<div class="msg_a">' + userName + ':&nbsp;&nbsp;' + message + '</div>');
+        var height = $('.msg_body')[0].scrollHeight;
+        $('.msg_body').scrollTop(height);
+    }
+
+    //The hub uses this function to broadcast messages.
+    chat.client.broadcastMessage = function (name, message) {
+        // Html encode display name and message.
+        var encodedName = $('<div />').text(name).html();
+        var encodedMsg = $('<div />').text(message).html();
+
+        // Add the message to the page.
+        //The user's own messages use div msg_b
+        if (encodedName == ($('#userName').val())) {
+            $('.msg_body').append('<div class="msg_b">' + encodedName
+                + ':&nbsp;&nbsp;' + encodedMsg + '</div>');
+        }
+            //Messages from other users use div msg_a
+        else {
+            $('.msg_body').append('<div class="msg_a">' + encodedName
+                + ':&nbsp;&nbsp;' + encodedMsg + '</div>');
+        }
+        $('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+    };
+}
+
+function registerEvents(chat) {
+    $('textarea').keypress(
+    function (e) {
+        if (e.keyCode == 13) {
+            var msg = $(this).val();
+            $(this).val('');
+            if (msg != '') {
+                // Call the Send method on the hub.
+                chat.server.send($('#userName').val(), msg);
+
+                // Clear text box and reset focus for next comment.
+                $('.msg_input').val('').focus();
+            }
+        }
+    });
+}
+
+function AddUser(chat, id, name) {
+    var userId = $('#userId').val();
+    var code = "";
+    if (userId == id) {
+        code = $('<div id="' + id + '" class="user" >' + name + '</div>');
+        $(code).dblclick(function () {
+            var id = $(this).attr('id');
+            if (userId == id) {
+                $('.msg_wrap').show();
+                $('.msg_box').show();
+            }
+        });
+    }
+    else {
+        code = $('<div id="' + id + '" class="user" >' + name + '</div>');
+        $(code).dblclick(function () {
+            var id = $(this).attr('id');
+            if (userId != id) {
+                alert('open Private Chat Window');
+                //OpenPrivateChatWindow(chatHub, id, name);
+            }
+        });
+    }
+    $(".chat_body").append(code);
+}
+
+window.onload = function () {
+    $('.login').dialog({
+        title: 'Login',
+        width: 430,
+        height: 250,
+        buttons: {
+            Close:
+                function () {
+                    $(this).dialog('close');
+                }
+        }
+    });
+}
